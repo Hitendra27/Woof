85a6b23e73150c48a72ed56e8507cb4ff2988ee9
Hitendra Quessou <73651340+Hitendra27@users.noreply.github.com>
2024-03-12 18:18:26 +0000

    Update Team.kt

--- app/src/main/java/com/example/woof/data/Team.kt
+++ app/src/main/java/com/example/woof/data/Team.kt
@@ -1,1 +1,12 @@
+data class Dog(
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val name: Int,
+    val age: Int,
+    @StringRes val hobbies: Int
+)
 
+data class Team(
+  @DrawableRes val imageResourceId: Int,
+  @StringRes val name: Int,
+  
+  )
