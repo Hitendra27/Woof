dfa69828b50b6d1119bcd41ebd59b805f2a3407e
Hitendra Quessou <73651340+Hitendra27@users.noreply.github.com>
2024-03-26 19:46:36 +0000

    Update Movie.kt

--- app/src/main/java/com/example/woof/data/Movie.kt
+++ app/src/main/java/com/example/woof/data/Movie.kt
@@ -5,6 +5,34 @@ data class Movie(
   @StringRes val movieDescription: Int
 )
 
-<string>
-</string>
-
+<resources>
+    <string name="movie_1">Terminator 2: Judgment Day</string>
+   <string name="movie_1_dir">James Cameron</string>
+   <string name="movie_1_description">A cyborg, identical to the one who failed to kill Sarah Connor, must now protect her ten year old son John from an even more advanced and powerful cyborg.</string>
+   <string name="movie_2">Reservoir Dogs</string>
+   <string name="movie_2_dir">Quentin Tarantino</string>
+   <string name="movie_2_description">When a simple jewelry heist goes horribly wrong, the surviving criminals begin to suspect that one of them is a police informant.</string>
+  <string name="movie_3">The Hangover</string>
+   <string name="movie_3_dir">Todd Phillips</string>
+   <string name="movie_3_description">Three buddies wake up from a bachelor party in Las Vegas, with no memory of the previous night and the bachelor missing. They make their way around the city in order to find their friend before his wedding.</string>
+ <string name="movie_4">The Matrix</string>
+   <string name="movie_4_dir">The Wachowskis</string>
+   <string name="movie_4_description">When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.</string>
+   <string name="movie_5">The Shawshank Redemption</string>
+   <string name="movie_5_dir">Frank Darabont</string>
+   <string name="movie_5_description">Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.</string>
+   <string name="movie_6">The Godfather</string>
+   <string name="movie_6_dir">Francis Ford Coppola</string>
+   <string name="movie_6_description">The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.</string>
+  <string name="movie_7">The Dark Knight</string>
+   <string name="movie_7_dir">Christopher Nolan</string>
+   <string name="movie_7_description">When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.</string>
+  <string name="movie_8"></string>
+   <string name="movie_8_dir"></string>
+   <string name="movie_8_description"></string>
+  
+  
+   <string name="movie_2"></string>
+   <string name="movie_2_dir"></string>
+   <string name="movie_1"></string>
+</resources>
